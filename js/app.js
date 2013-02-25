78c1b6585cffa85bcc38d6a068e7af5b26828cf5
Alexander Itkin <alexander.s.itkin@gmail.com>
2013-02-24 19:33:22 -0600

    read in cities from json file

--- js/app.js
+++ js/app.js
@@ -1,47 +1,4 @@
-var utcOffsetToCity = {
-    "-12.0" : ["Baker Island", "Howland Island"],
-    "-11.0" : ["Jarvis Island", "Niue"],
-    "-10.0" : ["Honolulu", "Papeete", "Avarua"],
-    "-9.5" : ["Marquesas Islands"],
-    "-9.0" : ["Anchorage", "Fairbanks", "Gambier Islands"],
-    "-8.0" : ["Tijuana", "Los Angeles", "Vancouver", "Seattle"],
-    "-7.0" : ["Phoenix", "Calgary", "Ciudad Juárez", "Cheyenne, Wyoming"],
-    "-6.0" : ["Chicago", "Guatemala City", "Mexico City", "San José", "San Salvador", "Winnipeg", "Managua"],
-    "-5.0" : ["Ann Arbor", "New York City", "Havana", "Toronto"],
-    "-4.5" : ["Caracas", "Maracay", "Valencia,_Carabobo"],
-    "-4.0" : ["Santiago", "La Paz", "San Juan de Puerto Rico", "Manaus", "Halifax"],
-    "-3.5" : ["St. John\"s", "Corner Brook"],
-    "-3.0" : ["Buenes Aires", "Montevideo", "Sao Paulo", "Catamarca"],
-    "-2.0" : ["South Sandwhich Islands", "Fernando de Noronha"],
-    "-1.0" : ["Cape Verde", "Ittoqqortoormiit"],
-    "0.0" : ["Lisbon", "London", "Abidjan", "Dublin", "Liverpool"],
-    "1.0" : ["Amsterdam", "Berlin", "Stockholm", "Zurich", "Warsaw"],
-    "2.0" : ["Athens", "Kiev", "Beirut", "Helsinki"],
-    "3.0" : ["Nairobi", "Minsk", "Baghdad", "Doha"],
-    "3.5" : ["Tehran", "Karaj", "Mashhad"],
-    "4.0" : ["Moscow", "Kazan", "Dubai", "Yerevan"],
-    "4.5" : ["Kabul", "Kandahar"],
-    "5.0" : ["Karachi", "Tashkent", "Malé"],
-    "5.5" : ["New Delhi", "Colombo"],
-    "5.75" : ["Kathmandu"],
-    "6.0" : ["Dhaka", "Almaty"],
-    "6.5" : ["Yangon", "Naypyidaw"],
-    "7.0" : ["Bangkok", "Nobosibirsk", "Jakarta", "Hanoi"],
-    "8.0" : ["Perth", "Beijing", "Singapore"],
-    "8.75" : ["Eucla"],
-    "9.0" : ["Seoul", "Tokyo", "Irkutsk"],
-    "9.5" : ["Adelaide"],
-    "10.0" : ["Yakutsk", "Canberra"],
-    "10.5" : ["Lord Howe Island"],
-    "11.0" : ["Vladivostok", "Noumea"],
-    "11.5" : ["Norfolk Island"],
-    "12.0" : ["Auckland"],
-    "12.75" : ["Chatham Islands"],
-    "13.0" : ["Samoa"],
-    "14.0" : ["Line Islands"]
-};
-
-function getUtcOffset(neededTimeAdjustment){
+function getUtcOffset(utcOffsetToCity, neededTimeAdjustment){
     var utcOffsetToReturn;
     $.each(utcOffsetToCity, function(utcOffset, city) {
         if (neededTimeAdjustment <= utcOffset) {
@@ -66,14 +23,14 @@ function getWikiLinkForCity(cityName){
     return "<a href=" + wikiUrlBase + cityNameNoSpaces + ">" + cityName + "</a>";
 }
 
-function get5pmLocationAndTime()
+function get5pmLocationAndTime(utcOffsetToCity)
 {
     var currentTime = new Date();
     var hour = currentTime.getUTCHours();
     var minutes = currentTime.getMinutes();
     if ((17 - hour) > 12) hour += 24;
     var neededTimeAdjustment = 17 - hour;
-    var utcOffset = getUtcOffset(neededTimeAdjustment);
+    var utcOffset = getUtcOffset(utcOffsetToCity, neededTimeAdjustment);
     var offsetTime = getOffsetTime(utcOffset, hour, minutes);
     var randomNumber = Math.floor(Math.random() * utcOffsetToCity[utcOffset].length)
     var offsetCity = utcOffsetToCity[utcOffset][randomNumber];
@@ -83,21 +40,25 @@ function get5pmLocationAndTime()
     return "It is " + offsetTime + "pm " + prepositionWord + " " + wikiLink + ".";
 }
 
-jQuery(document).ready(function() {
-  jQuery(".about_content").hide();
-  jQuery(".about_heading").click(function()
-  {
-    jQuery(this).next(".about_content").slideToggle(200);
-  });
-});
+function setAboutHeadingToggle(){
+    jQuery(document).ready(function() {
+      jQuery(".about_content").hide();
+      jQuery(".about_heading").click(function()
+      {
+        jQuery(this).next(".about_content").slideToggle(200);
+      });
+    });    
+}
 
-var mainParagraph = document.getElementById("main_paragraph");
-mainParagraph.innerHTML = get5pmLocationAndTime();
-/*setInterval(function(){
-    mainParagraph.innerHTML = get5pmLocationAndTime();
-}, 7000);*/
-
-$.getJSON("http://5oclock.info/cities.jsoncallback=?", function(data) {
-    var temp = data;
+setAboutHeadingToggle();
+$.getJSON("http://5oclock.info/cities.json", function(utcOffsetToCity) {
+    var mainParagraph = document.getElementById("main_paragraph");
+    mainParagraph.innerHTML = get5pmLocationAndTime(utcOffsetToCity);
+    setInterval(
+        function()
+        {
+            mainParagraph.innerHTML = get5pmLocationAndTime(utcOffsetToCity);
+        },
+        7000);
 });
 
